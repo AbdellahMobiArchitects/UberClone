66b80a32064acdc47627ed0a681c2ffa2d44c65f
AbdellahOusrhire <Abdellah.Ousrhire@astuciagroup.ma>
2017-11-11 15:15:23 +0000

    go

--- UberClone.Web/Models/UberCloneModel.Designer.cs
+++ UberClone.Web/Models/UberCloneModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Xamarin\source\repos\UberClone\UberClone.Web\Models\UberCloneModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Xamarin\Source\Repos\UberClone\UberClone.Web\Models\UberCloneModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
